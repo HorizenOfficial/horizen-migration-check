82461a58e6714eb543a3d12c26ba33ae0d342057
paolocappelletti <paolo@horizenlabs.io>
2025-04-03 12:20:40 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+## Horizen migration check script
+
+Command line script to verify correctness of Horizen migration data.
+
+
+### Prerequisites:
+
+- Execute a full dump of ZEND and EON following these instructions: [ TODO add link to docs website ]
+- RPC connection to a BASE node, and addresses of the official EON and ZEND Vault contracts
+- NodeJS environment locally installed
+
+### Instructions:
+
+- Download this project and install the dependencises with:
+
+   ```
+   npm init
+    ```
+
+- Rename .env.template file into .env and complete the nested properties.
+
+
+- Execute with:
+
+   ```
+   node index.js
+    ```
+
+- Thre script will calculate locally a cumulative hash from the dumps, and compare it to the one stored onchain in the vault smart contracts.<br>
+If everything goes well, you will obtain a log like this in the console:
+
+   ```
+   Checking EON balances
+   Hash computed locally: 0xbae9810c922613c3eb3364fe76bd19736472a3afb490e2772adfc25605479981
+   Hash from contract: 0xbae9810c922613c3eb3364fe76bd19736472a3afb490e2772adfc25605479981
+   ✅ EON balances verified correctly
+   Checking ZEND balances
+   Hash computed locally: 0x24afbbf6c451bc5e719e198d45cb97e084d6fddf0a3f1d203badda5a1635ea13
+   Hash from contract: 0x24afbbf6c451bc5e719e198d45cb97e084d6fddf0a3f1d203badda5a1635ea13
+   ✅ ZEND balances verified correctly
+   ✅ All balances are correct!
+    ```
+
---
+++ check.js
@@ -0,0 +1,104 @@
+import JSONbig from "json-bigint";
+import fs from "fs";
+import Web3 from "web3";
+const jsonBig = JSONbig({ storeAsString: true });
+
+const ABI = [
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "_cumulativeHash",
+      "outputs": [
+        {
+          "name": "",
+          "type": "bytes32"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ];
+
+var web3;
+
+export default async function doCheck() {
+    console.log("Horizen migration check script");
+    console.log("Using the following parameters:");
+    console.log("ZEND_DUMP: " +process.env.ZEND_DUMP);
+    console.log("EON_DUMP: " +process.env.EON_DUMP);
+    console.log("NETWORK: " +process.env.NETWORK);   
+    console.log("ZEND_VAULT_CONTRACT: " +process.env.ZEND_VAULT_CONTRACT);  
+    console.log("EON_VAULT_CONTRACT: " +process.env.EON_VAULT_CONTRACT);   
+    console.log("\n"); 
+    web3 = new Web3(new Web3.providers.HttpProvider(process.env.NETWORK));
+
+    console.log("Checking EON balances");
+    var jsonFile = fs.readFileSync(process.env.EON_DUMP).toString('utf-8');
+    var jsonData = jsonBig.parse(jsonFile);
+    var tuples = Object.entries(jsonData).map(([key, value]) => [key, value.toString()]);  
+    tuples.sort((a, b) => a[0].localeCompare(b[0]));
+    var hash = calculateHashEON(tuples);
+    console.log("Hash computed locally:", hash);
+
+    var vaultEONContract = new web3.eth.Contract(ABI, process.env.EON_VAULT_CONTRACT);
+    var cumulativeHash = await vaultEONContract.methods._cumulativeHash().call();
+    console.log('Hash from contract:', cumulativeHash);
+    if(cumulativeHash != hash){
+      console.error("[ERROR] EON balances not confrming with hash published by: "+process.env.EON_VAULT_CONTRACT);
+      return false;
+    }else{
+      console.log("\u2705 EON balances verified correctly");
+    }
+
+    console.log("Checking ZEND balances");
+    jsonFile = fs.readFileSync(process.env.ZEND_DUMP).toString('utf-8');
+    jsonData = jsonBig.parse(jsonFile);
+    var tuples = Object.entries(jsonData).map(([key, value]) => [key, value.toString()]);  
+    tuples.sort((a, b) => a[0].localeCompare(b[0]));
+    var hash = calculateHashZEND(tuples);
+    console.log("Hash computed locally:", hash);
+
+    var vaultEONContract = new web3.eth.Contract(ABI, process.env.ZEND_VAULT_CONTRACT);
+    var cumulativeHash = await vaultEONContract.methods._cumulativeHash().call();
+    console.log('Hash from contract:', cumulativeHash);
+    if(cumulativeHash != hash){
+      console.error("[ERROR] ZEND balances not conforming with hash published by: "+process.env.ZEND_VAULT_CONTRACT);
+      return false;
+    }else{
+      console.log("\u2705 ZEND balances verified correctly");
+    }
+    console.log("\u2705 All balances are correct!");
+
+    return true;
+}
+
+function calculateHashEON(tuples){
+  var dumpRecursiveHash = "0x0000000000000000000000000000000000000000000000000000000000000000";
+  for (const [key, value] of tuples) {
+    dumpRecursiveHash = updateCumulativeHashEON(dumpRecursiveHash, key, value);
+  }
+  return dumpRecursiveHash;    
+}
+
+function updateCumulativeHashEON(previousHash, address, value){
+    //the following hashing algorithm produces the same output as the one used in solidity
+    const encoded = web3.eth.abi.encodeParameters(['bytes32', 'address', 'uint256'],[previousHash, address, value])
+    return web3.utils.sha3(encoded, {encoding: 'hex'})
+}
+
+function calculateHashZEND(tuples){
+  var dumpRecursiveHash = "0x0000000000000000000000000000000000000000000000000000000000000000";
+  for (const [key, value] of tuples) {
+    dumpRecursiveHash = updateCumulativeHashZEND(dumpRecursiveHash, key, value);
+  }
+  return dumpRecursiveHash;    
+}
+
+function updateCumulativeHashZEND(previousHash, address, value){
+  //the following hashing algorithm produces the same output as the one used in solidity
+  const encoded = web3.eth.abi.encodeParameters(['bytes32', 'bytes20', 'uint256'],[previousHash, address, value])
+  return web3.utils.sha3(encoded, {encoding: 'hex'})
+}
+
+
